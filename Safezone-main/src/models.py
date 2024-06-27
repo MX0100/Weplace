46f6729a66aeed7ce10440211519e82a1c1b6224
MX0100 <93124355+MX0100@users.noreply.github.com>
2024-06-27 04:19:43 -0230

    Update models.py

--- Safezone-main/src/models.py
+++ Safezone-main/src/models.py
@@ -1,17 +1,20 @@
-from datetime import datetime, timedelta
 import uuid
+from datetime import datetime, timedelta
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
 
+
 class Content(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.Text, nullable=False)
+
 
 class UserSession(db.Model):
     id = db.Column(db.Integer, primary_key=True)
